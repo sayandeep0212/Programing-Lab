f35d787317ed0153211278fc106cff763907d521
Sayandeep Pradhan <sayandeep0212@gmail.com>
2025-09-14 12:50:27 +0530

    Update question26.c
    There is some problem in past uploaded file's code. So that i updated the code.

--- C Programming Day 5/question26.c
+++ C Programming Day 5/question26.c
@@ -1,26 +1,34 @@
 #include<stdio.h>
-int main() {
-    float a,b,x;
-    char o;
-    printf("ENTER YOUR FIRST NUMBER:");
-    scanf("%f", &a);
-    printf("ENTER YOUR SECOND NUMBER:");
-    scanf("%f", &b);
-    printf("Choose Your Operand Like +,-,*,/:");
-    scanf(" %c", &o);
 
-    switch(o) {
-        case'+': x=a+b;
-            break;
-        case'-': x=a-b;
-            break;
-        case'*': x=a*b;
-            break;
-        case'/': x=a/b;
-            break;
-        default:
-            printf("Wrong Input");
-    }
-    printf("YOUR FINAL RESULT IS: %f", x);
-    return 0;
+int main(){
+
+        float a,b;
+        char o;
+        printf("Enter The First Number: ");
+        scanf("%f", &a);
+        printf("Enter The Second Number: ");
+        scanf("%f", &b);
+        printf("Enter The Operator: ");
+        scanf(" %c",&o);
+
+        switch(o){
+                case '+':
+                        printf("%f",a+b);
+                        break;
+                case '-':
+                        printf("%f",a-b);
+                        break;
+                case '*':
+                        printf("%f",a*b);
+                        break;
+                case '/':
+                        printf("%f",a/b);
+                        break;
+
+                default: 
+                        printf("Wrong input");
+                        break;
+
+        }
+
 }

a4199cf42925a44d15219b78000f2e43a71df738
Sayandeep Pradhan <sayandeep0212@gmail.com>
2025-11-08 02:15:57 +0530

    update
    Added myStrcat function to concatenate two strings.

--- C Programming Day 8/question55.c
+++ C Programming Day 8/question55.c
@@ -1,6 +1,5 @@
 #include <stdio.h>
 
-// Function to find the length of a string
 int myStrlen(const char *str) {
     int length = 0;
     while (str[length] != '\0') {
@@ -9,42 +8,47 @@ int myStrlen(const char *str) {
     return length;
 }
 
-// Function to copy a string from src to dest
 void myStrcpy(char *dest, const char *src) {
     int i = 0;
     while (src[i] != '\0') {
         dest[i] = src[i];
         i++;
     }
-    dest[i] = '\0'; // Null terminate
+    dest[i] = '\0'; 
 }
 
-// Function to compare two strings
 int myStrcmp(const char *str1, const char *str2) {
     int i = 0;
     while (str1[i] == str2[i]) {
         if (str1[i] == '\0') {
-            return 0; // Strings are equal
+            return 0; 
         }
         i++;
     }
-    return str1[i] - str2[i]; // Difference of first non-matching chars
+    return str1[i] - str2[i]; 
+}
+
+void myStrcat(char *dest, const char *src) {
+    int i = 0;
+    while (dest[i] != '\0') i++;
+    int j = 0;
+    while (src[j] != '\0') {
+        dest[i++] = src[j++];
+    }
+    dest[i] = '\0';
 }
 
 int main() {
-    // 1. Test myStrlen
     char str1[] = "Hello";
     int len = myStrlen(str1);
     printf("1. Length of \"%s\" is: %d\n\n", str1, len);
 
-    // 2. Test myStrcpy
     char str2[] = "Programming";
     char destination[20];
     myStrcpy(destination, str2);
     printf("2. Original string: \"%s\"\n", str2);
     printf("   Copied string:  \"%s\"\n\n", destination);
 
-    // 3. Test myStrcmp
     char s3[] = "Apple";
     char s4[] = "Apple";
     char s5[] = "Banana";
@@ -53,18 +57,22 @@ int main() {
     int cmp2 = myStrcmp(s3, s5);
 
     printf("3. Comparing \"%s\" and \"%s\": ", s3, s4);
-    if (cmp1 == 0) {
+    if (cmp1 == 0)
         printf("Strings are equal.\n");
-    } else {
+    else
         printf("Strings are not equal.\n");
-    }
 
     printf("   Comparing \"%s\" and \"%s\": ", s3, s5);
-    if (cmp2 == 0) {
+    if (cmp2 == 0)
         printf("Strings are equal.\n");
-    } else {
+    else
         printf("Strings are not equal.\n");
-    }
+
+    char x[50] = "Good";
+    char y[] = "Morning";
+    printf("\n4. Before concatenation: x = \"%s\", y = \"%s\"\n", x, y);
+    myStrcat(x, y);
+    printf("   After concatenation: x = \"%s\"\n", x);
 
     return 0;
 }
